9c78826c906f832ee66dc59cabf8b916fa5c2a17
Denis Moskowitz <denismm@gmail.com>
2022-08-26 16:06:23 -0400

    use python3 instead of python

--- Gui.py
+++ Gui.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from tkinter import *
 from Puzzle import Puzzle
 from Grid import Grid
--- testgrid.py
+++ testgrid.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from Grid import Grid
 from Puzzle import Puzzle
