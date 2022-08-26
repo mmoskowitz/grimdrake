08e0ad6545ec1ec9d679115929ed36f4e27b92eb
Denis Moskowitz <denismm@gmail.com>
2022-08-26 15:50:58 -0400

    2to3 and black

--- Puzzle.py
+++ Puzzle.py
@@ -1,30 +1,28 @@
 #!/usr/bin/env python
 
 import copy
-from Grid import Grid
-from Gridder import Gridder
-from Filler import Filler
+from .Grid import Grid
+from .Gridder import Gridder
+from .Filler import Filler
 
 
 class Puzzle:
-	
-	def __init__(self, w, h, a,d):
-		self.grid = Grid(w,h,a,d)
-		self.gridder = Gridder(self)
-		self.filler = Filler(self.grid)
-		self.lastnumber = 1
+    def __init__(self, w, h, a, d):
+        self.grid = Grid(w, h, a, d)
+        self.gridder = Gridder(self)
+        self.filler = Filler(self.grid)
+        self.lastnumber = 1
 
+    def creategrid(self):
+        self.gridder.creategrid()
+        self.grid = self.gridder.grid
+        self.filler.grid = self.grid
 
-	def creategrid(self):
-		self.gridder.creategrid()
-		self.grid = self.gridder.grid
-		self.filler.grid = self.grid
+    def fill(self):
+        self.filler.fill()
 
-	def fill(self):
-		self.filler.fill()
-
-	def printwordlist(self):
-		print(self.filler.entries)
-		for row in range(0,grid.width):
-			for col in range(0,grid.height):
-				a = 1
+    def printwordlist(self):
+        print((self.filler.entries))
+        for row in range(0, grid.width):
+            for col in range(0, grid.height):
+                a = 1
--- grimdrake.py
+++ grimdrake.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 
-from Grid import Grid
-from Puzzle import Puzzle
+from .Grid import Grid
+from .Puzzle import Puzzle
 import sys
 import copy
-#import cProfile
+
+# import cProfile
 
 width = int(sys.argv[1])
 height = int(sys.argv[2])
 
-puzzle = Puzzle(width,height,None, None)
-#cProfile.run('puzzle.creategrid()')
+puzzle = Puzzle(width, height, None, None)
+# cProfile.run('puzzle.creategrid()')
 puzzle.creategrid()
 
 puzzle.fill()
-
-
-
